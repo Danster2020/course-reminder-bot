6ccfa4945808478c01ea0d3eaa295a1544f860d9
Danster <daniel.bleckert@me.com>
2025-08-16 23:25:04 +0200

    added docker

--- app/bot.py
+++ app/bot.py
@@ -5,19 +5,41 @@ import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 import logging
+from dotenv import load_dotenv
+from pathlib import Path
+
 
 import database
 
 # INIT
-reminder_dates_list = ["2025-05-08"]
+reminder_weeks_tuple = (28, 30, 34)  # weeks when to be notified
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='data.log', encoding='utf-8', level=logging.DEBUG)
-load_dotenv()
+logging.basicConfig(
+    filename='data.log',
+    encoding='utf-8',
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S %Z'
+)
+
+env_path = Path(__file__).resolve().parent.parent / "stack.env"
+load_dotenv(dotenv_path=env_path)
+TOKEN = os.getenv("TOKEN")
+
+if not TOKEN:
+    raise ValueError("TOKEN not found in environment variables!")
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
+
+
+def is_important_week(week):
+    if int(week) in reminder_weeks_tuple:
+        return True
+    return False
+
 
 async def send_message(message, user_message, is_private):
     try:
@@ -34,34 +56,44 @@ def is_private_message(interaction) -> bool:
     else:
         return False
 
+
 @tasks.loop(seconds=10)
+# runs every x seconds
 async def course_reminder_task():
-    current_date = str(datetime.datetime.now().date())
+    current_date = datetime.datetime.now().date()
+    current_week = current_date.isocalendar().week
     guilds: list[dict] = database.get_all_guilds()
-    
+
+    if not is_important_week(current_week):
+        return
+
     for guild in guilds:
         channel_id = guild.get("channel_id")
-        last_sent_date = str(guild.get("last_reminder_sent"))
+        last_sent_week = int(str(guild.get("last_reminder_sent")))
         guild_id = guild["guild_id"]
-    
+
         if channel_id:
             try:
                 channel = await client.fetch_channel(int(channel_id))
             except discord.NotFound:
-                logger.warning(f"Channel {channel_id} not found. It might have been deleted.")
+                logger.warning(
+                    f"Channel {channel_id} not found. It might have been deleted.")
                 continue
             except discord.Forbidden:
-                logger.warning(f"Cannot access channel {channel_id}. Missing permissions?")
+                logger.warning(
+                    f"Cannot access channel {channel_id}. Missing permissions?")
                 continue
             except Exception as e:
                 logger.error(f"Failed to fetch channel {channel_id}: {e}")
                 continue
 
             if channel:
-                if current_date != last_sent_date:
+                if int(current_week) != int(last_sent_week):
                     await channel.send("Nu kan du registrera dig på kurser/tentor!")
-                    logger.info(f"Reminder sent to server: {guild_id} in channel {channel_id}")
-                    database.set_last_reminder_sent(guild_id, current_date)
+                    logger.info(
+                        f"Reminder sent to server {guild_id} in channel {channel_id}")
+                    database.set_last_reminder_sent(
+                        guild_id, str(current_week))
             else:
                 logger.info(f"Channel: {channel_id} not found. Removed?")
         else:
@@ -77,7 +109,6 @@ def run_bot():
         startup_message = f'{client.user} is now running!'
         logger.info(startup_message)
         print(startup_message)
-
 
     @client.event
     async def on_message(message):
@@ -95,16 +126,17 @@ def run_bot():
         print(f"{username} said: '{user_message}' ({channel})")
 
         # await send_message(message, user_message, is_private=False)
-        
-    @tree.command(name="set_channel", description="Sets the channel for the bot to send reminders in.")
-    async def command(interaction):
+
+    # sets channel to receive reminders in
+    @tree.command(name="set_channel", description="Ställer in vilken kanal som ska ta emot påminnelser")
+    async def command(interaction: discord.Interaction, channel: discord.TextChannel):
         if is_private_message(interaction):
             await interaction.response.send_message("Command not allowed in DM.")
         else:
             guild_id = interaction.guild.id
-            channel_id = interaction.channel.id
+            channel_id = channel.id
             database.set_target_channel(guild_id, channel_id)
             print("guild_id:", guild_id, "channel_id", channel_id)
-            await interaction.response.send_message("test response")
-        
-    client.run(os.environ['TOKEN'])
+            await interaction.response.send_message(f"Påminnelser kommer nu att skickas till kanal <#{channel_id}>")
+
+    client.run(TOKEN)
--- app/database.py
+++ app/database.py
@@ -1,13 +1,13 @@
+import os
 from pymongo import MongoClient
 import logging
 logging.getLogger('pymongo').setLevel(logging.WARNING)
 
-client = MongoClient("localhost", 27017)
+mongo_uri = os.getenv("MONGO_URI", "mongodb://mongo:27017/courseReminderBotDB")
+client = MongoClient(mongo_uri)
 
 db = client.courseReminderBotDB
-
 guilds = db.guilds
-
 guilds_coll = db["guilds"]
 
 # guilds.insert_one({"guild_id": "1065289293733568582"})
@@ -19,34 +19,36 @@ guilds_coll = db["guilds"]
 
 for doc in guilds_coll.find():
     print(doc)
-    
+
 
 def get_or_create_guild(guild_id: str) -> dict:
     query = {"guild_id": str(guild_id)}
     guild = guilds_coll.find_one(query)
-    
+
     if guild is None:
         # Create a default document if not found
         new_guild = {
             "guild_id": str(guild_id),
             "channel_id": None,
-            "last_reminder_sent": None 
+            "last_reminder_sent": 0
         }
         guilds_coll.insert_one(new_guild)
         return new_guild
-    
+
     return guild
+
 
 def get_all_guilds() -> list[dict]:
     return list(guilds_coll.find())
 
-    
+
 def set_target_channel(guild_id: str, channel_id: str):
     get_or_create_guild(guild_id)
     query = {"guild_id": str(guild_id)}
     update = {"$set": {"channel_id": str(channel_id)}}
     guilds_coll.update_one(query, update, upsert=True)
-    
+
+
 def set_last_reminder_sent(guild_id: str, date: str):
     query = {"guild_id": str(guild_id)}
     update = {"$set": {"last_reminder_sent": str(date)}}
